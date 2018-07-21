99b40bc8271b6560c26c9cc52f6f863583653cbb
Sunim Acharya <sunim.54@gmail.com>
2018-07-21 19:28:59 +0545

    Setup for tech specs

--- classify.py
+++ classify.py
@@ -40,75 +40,6 @@ def load_graph(model_file):
         tf.import_graph_def(graph_def)
     return graph
 
-class Octopart:
-    """
-    Class to contain the Octopart data without saving a Json string
-    """
-    def __init__(self, jsonStream):
-        """
-        Octopart init. Takes a dict representing a single Octopart item and converts it to an Octopart()
-        :param jsonStream: Dict returned from an Octosearch
-        :type jsonStream: dict
-        """
-        self.uid = jsonStream['uid']
-        self.mpn = jsonStream['mpn']
-        self.brandName = jsonStream['brand']['name']
-        self.manufacturer = jsonStream['manufacturer']['name']
-        self.octopartUrl = jsonStream['octopart_url']
-        self.shortDescription = jsonStream['short_description']
-        self.quantity = 0
-
-    def getTechSpecs(self, apiKey, images=False, description=False, datasheets=False):
-        """
-        Performs a part-match with Octopart and includes the tech specs. Adds the tech specs to the Octopart
-        :param apiKey: API key from config file
-        :type apiKey: str
-        :param images: Boolean on whether or not to include images
-        :type images: bool
-        :param description: Boolean on whether or not to include long descriptions
-        :type description: bool
-        :param datasheets: Boolean on whether or not to include datasheet downloads
-        :type datasheets: bool
-        """
-        # To do: add auto datasheet download and images and description
-        url = 'http://octopart.com/api/v3/parts/'
-        url += self.uid
-        url += '?apikey='
-        url += "6aa164d3"
-        url += '&include[]=specs'
-        if images:
-            url += '&include[]=imagesets'
-        if description:
-            url += '&include[]=descriptions'
-        if datasheets:
-            url += '&include[]=datasheets'
-
-        data = json.loads(urllib.request.urlopen(url).read().decode())
-        if data['datasheets']:
-            if len(data['datasheets']) > 1:
-                urlString = data['datasheets'][1]['url']
-                urlArray = urlString.rpartition('/')
-                fileName = urlArray[len(urlArray) - 1]
-                fileName = os.path.join('datasheets/', fileName)
-                self.dataFile = fileName
-                self.dataURL = urlString
-
-        specJson = data['specs']
-        specArray = []
-        for specName in specJson:
-            tmpSpec = specJson[specName]
-            name = tmpSpec['metadata']['name']
-            value = tmpSpec['display_value']
-            specArray.append([name, value])
-
-        descArray = []
-        if data['descriptions']:
-            for i in range(0, len(data['descriptions'])):
-                descArray.append(data['descriptions'][i]['value'])
-
-        self.specs = specArray
-        self.descriptions = descArray
-
 @route('/classify_image/', method='POST')
 def index():
     response.content_type='application/json'
@@ -125,7 +56,7 @@ def index():
         path = save_image(info['image64'])
         jsonF = score(path)
 
-        # Octopart API
+        # Octopart API Searches the Octopart database 
         url = "http://octopart.com/api/v3/parts/search"
         url += "?apikey="
         url += "6aa164d3"
@@ -147,9 +78,36 @@ def index():
         manufacturer = jsonStream['manufacturer']['name']
         octopartUrl = jsonStream['octopart_url']
         shortDescription = jsonStream['short_description']
-        print(jsonStream['uid'])
+        
+        # Performs a part-match with Octopart and includes the tech specs
+        url2 = 'http://octopart.com/api/v3/parts/'
+        url2 += uid
+        url2 += '?apikey='
+        url2 += "6aa164d3"
+        url2 += '&include[]=specs'
+        url2 += '&include[]=imagesets'
+        url2 += '&include[]=descriptions'
+        data2 = json.loads(urllib.request.urlopen(url).read().decode())
+
+        specJson = data2['specs']
+        specArray = []
+
+        for specName in specJson:
+            tmpSpec = specJson[specName]
+            name = tmpSpec['metadata']['name']
+            value = tmpSpec['display_value']
+            specArray.append([name, value])
+
+        descArray = []
+        if data['descriptions']:
+            for i in range(0, len(data['descriptions'])):
+                descArray.append(data['descriptions'][i]['value'])
+
+        specs = specArray
+        descriptions = descArray
+
         #ident = { "Component": jsonF[0], "Predictions": jsonF[1], "Info": octoList }
-        ident = { "Component": jsonF[0], "Predictions": jsonF[1], "Info": shortDescription }
+        ident = { "Component": jsonF[0], "Predictions": jsonF[1], "ShortInfo": shortDescription, "octopartUrl": octopartUrl }
         os.remove(path)
     return dict(ident)
 
